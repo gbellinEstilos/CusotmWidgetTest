e48da3884baeb88e7509b968d4e884bc5a65ef15
gbellinEstilos <61689038+gbellinEstilos@users.noreply.github.com>
2021-06-30 16:25:34 +0200

    Update double_donut_webcomponent.js

--- double_donut_webcomponent.js
+++ double_donut_webcomponent.js
@@ -27,13 +27,8 @@
           host.draw();
         }
       };
-
-      if (this._firstUpdate) {
-        LoadLibsAfterUpdate(this);
-        this._firstUpdate = false;
-      } else {
-        this.draw();
-      }
+      LoadLibsAfterUpdate(this);
+      this._init = false;
     }
 
     //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -94,68 +89,68 @@
 
     draw() {
 
-        var dataset1 = [
-          { count: 10 },
-          { count: 20 },
-          { count: 30 },
-          { count: 40 }
-        ];
+      var dataset1 = [
+        { count: 10 },
+        { count: 20 },
+        { count: 30 },
+        { count: 40 }
+      ];
 
-        var dataset2 = [
-          { count: 5 },
-          { count: 15 },
-          { count: 25 },
-          { count: 35 },
-          { count: 45 }
-        ];
+      var dataset2 = [
+        { count: 5 },
+        { count: 15 },
+        { count: 25 },
+        { count: 35 },
+        { count: 45 }
+      ];
 
-        var width = 400;
-        var height = 400;
-        var donutWidth = 75;
-        var radius1 = Math.min(width, height) / 2;
-        var radius2 = radius1 - donutWidth;
+      var width = 400;
+      var height = 400;
+      var donutWidth = 75;
+      var radius1 = Math.min(width, height) / 2;
+      var radius2 = radius1 - donutWidth;
 
-        var color1 = d3.scale.category20();
-        var color2 = d3.scale.category20c();
+      var color1 = d3.scale.category20();
+      var color2 = d3.scale.category20c();
 
-        var svg = d3.select('#chart')
-          .append('svg')
-          .attr('width', width)
-          .attr('height', height);
-        var svg1 = svg.append('g')
-          .attr('transform', 'translate(' + (width / 2) +
-            ',' + (height / 2) + ')');
-        var svg2 = svg.append('g')
-          .attr('transform', 'translate(' + (width / 2) +
-            ',' + (height / 2) + ')');
+      var svg = d3.select('#chart')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+      var svg1 = svg.append('g')
+        .attr('transform', 'translate(' + (width / 2) +
+          ',' + (height / 2) + ')');
+      var svg2 = svg.append('g')
+        .attr('transform', 'translate(' + (width / 2) +
+          ',' + (height / 2) + ')');
 
-        var arc1 = d3.svg.arc()
-          .innerRadius(radius1 - donutWidth)
-          .outerRadius(radius1);
-        var arc2 = d3.svg.arc()
-          .innerRadius(radius2 - donutWidth)
-          .outerRadius(radius2);
+      var arc1 = d3.svg.arc()
+        .innerRadius(radius1 - donutWidth)
+        .outerRadius(radius1);
+      var arc2 = d3.svg.arc()
+        .innerRadius(radius2 - donutWidth)
+        .outerRadius(radius2);
 
-        var pie = d3.layout.pie()
-          .value(function (d) { return d.count; })
-          .sort(null);
+      var pie = d3.layout.pie()
+        .value(function (d) { return d.count; })
+        .sort(null);
 
-        var path1 = svg1.selectAll('path')
-          .data(pie(dataset1))
-          .enter()
-          .append('path')
-          .attr('d', arc1)
-          .attr('fill', function (d, i) {
-            return color1(i);
-          });
-        var path2 = svg2.selectAll('path')
-          .data(pie(dataset2))
-          .enter()
-          .append('path')
-          .attr('d', arc2)
-          .attr('fill', function (d, i) {
-            return color2(i);
-          });
+      var path1 = svg1.selectAll('path')
+        .data(pie(dataset1))
+        .enter()
+        .append('path')
+        .attr('d', arc1)
+        .attr('fill', function (d, i) {
+          return color1(i);
+        });
+      var path2 = svg2.selectAll('path')
+        .data(pie(dataset2))
+        .enter()
+        .append('path')
+        .attr('d', arc2)
+        .attr('fill', function (d, i) {
+          return color2(i);
+        });
 
       /*          if (this._tagContainer){
                       this._tagContainer.parentNode.removeChild(this._tagContainer);
