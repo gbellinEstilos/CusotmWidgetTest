e58d02e4e14bd2a7f4163cc119e8d9eec2cd8080
gbellinEstilos <gbellin@estilos.it>
2021-07-09 16:05:29 +0200

    Update double_donut_webcomponent.js

--- double_donut_webcomponent.js
+++ double_donut_webcomponent.js
@@ -112,7 +112,7 @@
       if (this.shadowRoot.querySelector('#chart').childElementCount !== 0)
         d3.select(this.shadowRoot.querySelector('#chart')).selectAll("*").remove();
       
-      if (data == {} || data == undefined) {
+      if (data == undefined || data.length == {}) {
         dataset1 = [
           { count: 10 },
           { count: 20 },
